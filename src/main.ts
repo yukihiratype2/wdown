56e4bd859bb88768df4e27119884dbb67a39218c
natsuki <yukihiratype2@live.com>
2019-11-23 21:56:58 +0800

    fix dep

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,47 @@
+
 import { app, BrowserWindow } from 'electron';
+import path from 'path';
 
 
 let mainWindow = null;
 const ENABLE_DEBUG = process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true';
 
-async function installExtensions() {
-  const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
-  return installExtension(REACT_DEVELOPER_TOOLS)
-}
+const { default: installExtension, REACT_DEVELOPER_TOOLS } = !ENABLE_DEBUG ? { default: null, REACT_DEVELOPER_TOOLS: null } : require('electron-devtools-installer');
+const electronDebug = !ENABLE_DEBUG ? null : require('electron-debug');
 
-if (ENABLE_DEBUG) {
+
+async function installExtensions() {
+  return installExtension(REACT_DEVELOPER_TOOLS);
 }
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
-})
+});
 
 app.on('ready', async () => {
   if (ENABLE_DEBUG) {
-    await installExtensions()
-    require('electron-debug')();
+    await installExtensions();
+    electronDebug();
   }
 
   mainWindow = new BrowserWindow({
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
     },
     show: false,
     width: 1027,
-    height: 728
-  })
+    height: 728,
+  });
 
-  mainWindow.loadURL('file://' + __dirname + '/index.html')
+  mainWindow.loadURL(`file://${path.join(__dirname, '/index.html')}`);
 
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
-  })
+  });
 
   mainWindow.on('closed', () => {
-    mainWindow = null
-  })
-})
+    mainWindow = null;
+  });
+});
