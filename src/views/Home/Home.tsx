dcab89a1a518d0bffc17e03b15ed2fb7e87c0c19
natsuki <yukihiratype2@live.com>
2019-11-28 13:00:06 +0800

    write readme

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.h1`
 background-color: red;
 `;
 
-const Home = () => (
-  <Wrapper>
-    nani
-  </Wrapper>
-);
+const Button = styled.button`
+color: green;
+`;
+
+const Home = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Wrapper>
+      <Button onClick={() => setCount(count + 1)}>lickk</Button>
+      {count}
+    </Wrapper>
+  );
+};
 
 export default Home;
